d000b957e11875e951b8d458ca69e9705bc29856
a.chuchalin <chuchalin92@mail.ru>
2017-09-11 19:08:31 +0300

    Add jdbc to test

--- tech.model/src/main/java/ru/chuchalin/tech/model/aTestModel.java
+++ tech.model/src/main/java/ru/chuchalin/tech/model/aTestModel.java
@@ -1,18 +1,21 @@
 package ru.chuchalin.tech.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import ru.chuchalin.tech.model.dao.GraphQLSchemaModel;
 
 public class aTestModel {
 
 	public static void main(String[] args) {
-		//try{
-		//	Class.forName("org.postgresql.Driver");
-		//	Connection conn = DriverManager.getConnection("jdbc:postgresql://194.87.214.229/TechCalDB", "a.chuchalin", "Achprocedure1@99.2");
-		//	System.out.println("true");
-		//	conn.close();
-		//}catch (Exception e) {
-		//	e.printStackTrace();
-		//}
+		try{
+			Class.forName("org.postgresql.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:postgresql://194.87.214.229/TechCalDB", "a.chuchalin", "Achprocedure1@99.2");
+			System.out.println("true");
+			conn.close();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 		//HibernateUtil hu = new HibernateUtil().init("194.87.214.229/TechCalDB", "a.chuchalin", "Achprocedure1@99.2");
 		GraphQLSchemaModel ql = new GraphQLSchemaModel(null);
 		System.out.println(ql.jsonResult("query {city {name}}"));
